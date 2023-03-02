e9132779150e00ecf629fee9949ec5f620fbb667
alperenkavukk <kavuk.67.76@gmail.com>
2023-03-02 10:43:32 +0300

    Initial Commit

---
+++ listWatchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  listWatchApp
+//
+//  Created by Alperen Kavuk on 2.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
